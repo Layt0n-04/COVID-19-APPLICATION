4b6940e29e3716f94b906cbe095c2f0a38058a00
Frank <dev.frankg@gmail.com>
2021-08-03 20:47:57 -0400

    adding tables and some formats for them

--- Upside.js
+++ Upside.js
@@ -25,6 +25,31 @@ inquirer
       };
   axios.request(options).then(function (response){
       console.log (response.data[0])
+      var countryTotalStats = response.data[0]
+      const data = [
+        ["Total Confirmed Cases:", "Total People Recovered:","Total Critical:", "Total Deaths:"],
+        [countryTotalStats.confirmed, countryTotalStats.recovered, 
+          countryTotalStats.critical, countryTotalStats.deaths]
+      ];
+      // table settings     
+      const config = {
+        columnDefault: {
+          width: 16,
+        },
+        columns: [
+          { alignment: 'right' },
+          { alignment: 'right' },
+          { alignment: 'right' },
+          { alignment: 'right' },
+          {truncate: 100}
+        ],
+        header: {
+          alignment: 'center',
+          content: 'Covid-19 Total stats for \n' + countryTotalStats.country.toUpperCase(),
+        },
+      }
+        // table being logged here 
+        console.log(table(data, config));
       inquirer
     .prompt([
     // Here we ask the user if they want to see the table
@@ -58,9 +83,17 @@ inquirer
         columnDefault: {
           width: 16,
         },
+        columns: [
+          { alignment: 'right' },
+          { alignment: 'right' },
+          { alignment: 'right' },
+          { alignment: 'right' },
+          {wrapWord: true},
+          {truncate: 100}
+        ],
         header: {
           alignment: 'center',
-          content: 'Covid-19 stats \n' + covidStats.country.toUpperCase(),
+          content: 'Covid-19 Daily stats \n' + covidStats.country.toUpperCase(),
         },
       }
       for (var i = 0; i < provinces.length; i++){
@@ -88,59 +121,88 @@ inquirer
                 }
               };              
               axios.request(options).then(function (response) {
-                console.log(response.data);
+                
+                var vaccineArr = response.data
+                const data = [
+                  ["Developer/Researcher:", "Category:", "Phase:","Description:",]
+                ];
+                // table settings     
+                const config = {
+                  columnDefault: {
+                    width: 17,
+                  },
+                  columns: [
+                    { alignment: 'right' },
+                    { alignment: 'right' },
+                    { alignment: 'right' },
+                    { alignment: 'right' },
+                    {truncate: 100},
+                    {wrapWord: true}
+                  ],
+                  header: {
+                    alignment: 'center',
+                    content: 'Covid-19 \n' + "Vaccine Info".toUpperCase(),
+                  },
+                }
+                for (var i = 0; i < vaccineArr.length; i++){
+                  var element = vaccineArr[i];
+                  data.push([element.developerResearcher, element.category, element.phase, element.description])
+                  };
+                  // table being logged here 
+                  console.log(table(data, config));
+
                 inquirer
-        .prompt([
-        // Here we ask the user if they want the forecast for the same country.
-        {
-            type: "confirm",
-            message: "Would you also like to see the current weather forecast in a area ?",
-            name: "forecast",
-            default: true
-        }
-         ]).then(function(whether){
-            if (whether.forecast){
-                inquirer
-                  .prompt([
-                  // Here we ask the user for what country they are looking for.
-                    {
-                   type: "input",
-                   message: "Please list the city and state you want the forecast for",
-                   name: "userInput"
-                    }
-              ]).then(function(response){
-                var options = {
-                  provider: "mapquest",
-                  apiKey: "v2rTncGTaPacUB47gDtAvBdFYs1ZbWGW"
-                };
-                
-                var geocoder = NodeGeocoder(options);
-                
-                // Get all elements in process.argv, starting from index 2 to the end
-                // Join them into a string to get the space delimited address
-                var address = response.userInput
-                
-                
-                // Then use the Google Geocoder to geocode the address
-                geocoder.geocode(address, function(err, data) {
-                  
-                 var address = data[0]
-                
-                  // Depending on what information is available for an address, build formatted search
-                  var search = address.city + ", " + address.stateCode
-                  
-                  // Run the weather package to search by either zip or city.
-                  weather.find({ search: search, degreeType: "F" },function(err, result) {
-                    // If there is insufficient data, notify the user.
-                    if (err) {
-                      console.log("\r\n\r\n\r\n");
-                
-                      console.log("Sorry we don't have enough data on that location! Try somewhere else.");
-                
-                      console.log("\r\n\r\n\r\n");
-                
-                      return;
-                    }
+                .prompt([
+                // Here we ask the user if they want the forecast for the same country.
+                {
+                    type: "confirm",
+                    message: "Would you also like to see the current weather forecast in a area ?",
+                    name: "forecast",
+                    default: true
+                }
+                ]).then(function(whether){
+                    if (whether.forecast){
+                        inquirer
+                          .prompt([
+                          // Here we ask the user for what country they are looking for.
+                            {
+                          type: "input",
+                          message: "Please list the city and state you want the forecast for",
+                          name: "userInput"
+                            }
+                    ]).then(function(response){
+                      var options = {
+                        provider: "mapquest",
+                        apiKey: "v2rTncGTaPacUB47gDtAvBdFYs1ZbWGW"
+                      };
+                      
+                      var geocoder = NodeGeocoder(options);
+                      
+                      // Get all elements in process.argv, starting from index 2 to the end
+                      // Join them into a string to get the space delimited address
+                      var address = response.userInput
+                      
+                      
+                      // Then use the Google Geocoder to geocode the address
+                      geocoder.geocode(address, function(err, data) {
+                        
+                      var address = data[0]
+                      
+                        // Depending on what information is available for an address, build formatted search
+                        var search = address.city + ", " + address.stateCode
+                        
+                        // Run the weather package to search by either zip or city.
+                        weather.find({ search: search, degreeType: "F" },function(err, result) {
+                          // If there is insufficient data, notify the user.
+                          if (err) {
+                            console.log("\r\n\r\n\r\n");
+                      
+                            console.log("Sorry we don't have enough data on that location! Try somewhere else.");
+                      
+                            console.log("\r\n\r\n\r\n");
+                      
+                            return;
+                          }
                 
                     // Then print the Weather information and complete Address
                     console.log("\r\n\r\n\r\n");
